67ce717b547243a484c3bca509f9ca3d02f5332c
이민정 <@DESKTOP-CMQ012O>
2021-05-10 09:02:19 +0900

    스프링component

---
+++ src/main/java/spring5_component_scan_study/spring/MemberListPrinter.java
@@ -0,0 +1,19 @@
+package spring5_component_scan_study.spring;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberListPrinter {
+	@Autowired
+	private MemberDao memberDao;
+	@Autowired
+	private MemberPrinter printer;
+
+	public void printAll() {
+		Collection<Member> members = memberDao.selectAll();
+		members.forEach(m -> printer.print(m));
+	}
+}
